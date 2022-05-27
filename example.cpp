ddd03fcdf83b695632c28916c205c8807f5a3a2f
Hassaan shah <60673928+Hassaantheman@users.noreply.github.com>
2022-05-27 23:30:44 +0100

    Create example.cpp

---
+++ example.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int age = 50;
+    string CharacterName = "Tom";
+
+    cout << "There once was a man named " << CharacterName << endl;
+    cout << "He was " << age << " years old" << endl;
+    CharacterName = "Mike";
+    cout << "He liked the name " << CharacterName << endl;
+    cout << "But he did not like being " << age<< endl;
+    return 0;
+}
